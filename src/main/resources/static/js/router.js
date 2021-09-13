9bcdbb58c1f95a9678abc5c66af8f4c0aa464a04
박경옥(Park-KyungOk)(banulp) <banulp@ncsoft.com>
2021-09-13 20:06:00 +0900

    router

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -15,7 +15,6 @@ const routes = [
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = VueRouter.createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: VueRouter.createWebHashHistory(),
     routes, // short for `routes: routes`
 })
